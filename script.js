b8f44a6b0cd84a37f045057c7a9bd14f02245848
Franz <franz.hanschmidt@posteo.de>
2021-05-10 18:55:21 +0200

    implement edit functionality

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-const listItems = [];
+// Edit function
+// Steps involved:
+// 1. onclick: grab the correct item from the array
+//  1.1 focus element and enable editing (contentEditable=true)
+//  1.2 when element loses focus, update array with new info
+
+// get all list items
+let listItems = document.querySelectorAll(".item");
+
+// get textfields to be edited
+const itemsTextfield = document.querySelectorAll(".item-textfield");
+
+// get edit buttons
+const editButtons = document.querySelectorAll(".edit");
+
+// function for making item textfields editable on click
+function clickEdit() {
+  this.parentElement.previousElementSibling.contentEditable = "true";
+  this.parentElement.previousElementSibling.focus();
+}
+
+// function for saving changes
+function leaveEdit() {
+  this.contentEditable = "false";
+  listItems = document.querySelectorAll(".item");
+  console.log(listItems[0].firstElementChild.innerHTML);
+}
+
+editButtons.forEach((button) => button.addEventListener("click", clickEdit));
+itemsTextfield.forEach((button) => button.addEventListener("blur", leaveEdit));
+console.log(listItems);
