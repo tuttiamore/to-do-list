4974a5516940f79298951520ff821738415f5816
Franz <franz.hanschmidt@posteo.de>
2021-05-11 10:07:57 +0200

    update design

--- script.js
+++ script.js
@@ -7,25 +7,48 @@
 // get all list items
 let listItems = document.querySelectorAll(".item");
 
-// get textfields to be edited
-const itemsTextfield = document.querySelectorAll(".item-textfield");
-
-// get edit buttons
-const editButtons = document.querySelectorAll(".edit");
-
 // function for making item textfields editable on click
 function clickEdit() {
   this.parentElement.previousElementSibling.contentEditable = "true";
   this.parentElement.previousElementSibling.focus();
 }
 
-// function for saving changes
+// function for saving changes: make textfield not editable and update array
 function leaveEdit() {
   this.contentEditable = "false";
   listItems = document.querySelectorAll(".item");
-  console.log(listItems[0].firstElementChild.innerHTML);
 }
 
-editButtons.forEach((button) => button.addEventListener("click", clickEdit));
-itemsTextfield.forEach((button) => button.addEventListener("blur", leaveEdit));
-console.log(listItems);
+function editItemsWrapper() {
+  // get textfields to be edited
+  let itemsTextfield = document.querySelectorAll(".item-textfield");
+
+  // get edit buttons
+  let editButtons = document.querySelectorAll(".edit");
+
+  //add event listener and tasks
+  editButtons.forEach((button) => button.addEventListener("click", clickEdit));
+  itemsTextfield.forEach((button) =>
+    button.addEventListener("blur", leaveEdit)
+  );
+
+  //save changes, i.e. update itemList
+  document.querySelectorAll(".item");
+}
+
+editItemsWrapper();
+
+// Try out: Add new item
+// const addButton = document.querySelector(".input-button");
+
+// function addElement() {
+//   let mainList = document.querySelector(".item-wrapper");
+//   let newItem = listItems[0].cloneNode(true);
+//   let newInput = document.querySelector(".input-item").innerHTML;
+//          selecting innerHTML from input elements doesn't work
+//   newItem.firstElementChild.innerHTML = "newInput";
+//   mainList.append(newItem);
+//   editItemsWrapper();
+// }
+
+// addButton.addEventListener("click", addElement);
