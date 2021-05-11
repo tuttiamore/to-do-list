f83e2760bed0a1fc02c95c66ad80f9072862817b
claurennt <c.benedetti1989@gmail.com>
2021-05-11 17:27:34 +0200

    refactor create operation

--- script.js
+++ script.js
@@ -1,37 +1,34 @@
 const addedListItems = [];
 
+const itemWrapper = document.querySelector(".item-wrapper");
 
-// function to create a new list item, display it on the page as well as push it to the addedListItems array
-const addElementToList = () => {
-  let newAddedItem = document.querySelector(".input-item").value
-  addedListItems.push(newAddedItem)
-  let newArticle = document.createElement("article")
-  newArticle.setAttribute("class", "item")
-  newArticle.innerHTML = (
-      ` <div class="item-textfield">${newAddedItem}</div>
-      <div class="button-wrapper">
-        <button class="edit item-button">&#128393;</button>
-        <button class="check item-button">&#128504;</button>
-        <button class="delete item-button">&#10007;</button>
-      </div>`
-  )
-  document.querySelector(".item-wrapper").prepend(newArticle)
+// create new article with the user inserted item and display it on the page
+function addElementToArrayandPageList() {
+  let newAddedItem = document.querySelector(".input-item").value;
+  addedListItems.push(newAddedItem);
+  const itemTemplate = `
+          <article class="item">
+              <div class="item-textfield">${newAddedItem}</div>
+              <div class="button-wrapper">
+                <button class="edit item-button">&#128393;</button>
+                <button class="check item-button">&#128504;</button>
+                <button class="delete item-button">&#10007;</button>
+              </div>
+              </article>`;
+  itemWrapper.insertAdjacentHTML("afterbegin", itemTemplate);
 }
 
+let addButton = document.querySelector("#input-button");
 
-
-
-let addButton = document.querySelector("#input-button")
 // trigger function on click
-addButton.addEventListener("click", addElementToList)
+addButton.addEventListener("click", addElementToArrayandPageList);
 
+let inputField = document.querySelector(".input-item");
 
-
-
-let inputField = document.querySelector(".input-item")
+const onEnter = (event) => {
+  if (event.key === "Enter") {
+    addElementToArrayandPageList();
+  }
+};
 // trigger function on enter
-inputField.addEventListener("keydown", (event) => {
-  if (event.key === 'Enter') {
-    addElementToList()
-  } 
-})
+inputField.addEventListener("keydown", onEnter);
